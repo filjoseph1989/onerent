c2f8b806f6478130f8b3fbe4fce9664ae41b1fb4
Fil <fil@greyandgreentech.com>
2016-11-06 09:57:14 +0800

    added onerent files

---
+++ js/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>One Rent Comp Analysis</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css" media="screen" title="no title">
+    <script src="js/bootstrap.js" charset="utf-8"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
